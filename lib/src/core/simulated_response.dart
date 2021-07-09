cf6d3d4d473d69222149daa90b7daaa4dab5532a
Hoylen Sue <hoylen@hoylen.com>
2021-07-09 12:04:46 +1000

    Added additional body methods to the SimulatedResponse class.

--- lib/src/core/simulated_response.dart
+++ lib/src/core/simulated_response.dart
@@ -1,8 +1,21 @@
 part of core;
 
 //################################################################
+/// The source format for the body in a simulated response.
+///
+/// See [SimulatedResponse] for details and the [SimulatedResponse.bodyType]
+/// getter.
 
-enum _SimulatedResponseBody { none, bytes, string }
+enum SimulatedResponseBodyType {
+  /// There is no body.
+  none,
+
+  /// The source format of the body was a sequence of bytes.
+  bytes,
+
+  /// The source format of the body was a String.
+  string
+}
 
 //################################################################
 /// Response returned by simulations.
@@ -12,6 +25,30 @@ enum _SimulatedResponseBody { none, bytes, string }
 /// This class extends [Response] with methods to retrieve the body of the
 /// HTTP response. The other response classes (used by request handlers to
 /// produce HTTP responses) only allow the body to be produced.
+///
+/// **Body**
+///
+/// The body of the simulated response can be retrieved as a sequence of bytes
+/// using [bodyBytes], or as a String using either [bodyStr] or [bodyString].
+///
+/// The Handler that produced the response could have provided the body as
+/// a sequence of bytes, a String, or not provided any body at all. The
+/// simulated response internally stores any body in its source format.
+///
+/// The retrieval methods encode/decode the source into the desired format,
+/// if needed. If the source was a String, then _bodyString_
+/// does not perform any encoding; but _bodyBytes_ will require encoding the
+/// string value into bytes. If the source was a sequence of bytes, then
+/// _bodyBytes_ does not perform any decoding; but retrieving it with
+/// _bodyString_ will require decoding those bytes into a String.
+/// The _bodyStr_ getter is a convenience method for invoking _bodyString_
+/// with the UTF-8 encoding.
+///
+/// The [bodyIsEmpty] getter indicates if the body has content or not.
+/// This is more efficient than retrieving the body and then checking if it
+/// is empty or not, since no encoding/decoding will be performed.
+///
+/// The source format of the body can be determined from the [bodyType].
 
 class SimulatedResponse extends Response {
   //================================================================
@@ -67,15 +104,15 @@ class SimulatedResponse extends Response {
     if (core.hasBody) {
       if (core.hasBodyString) {
         _bodyStr = core.bodyStr();
-        _body = _SimulatedResponseBody.string;
+        _body = SimulatedResponseBodyType.string;
       } else if (core.hasBodyBytes) {
         _bodyBytes = core.bodyBytes();
-        _body = _SimulatedResponseBody.bytes;
+        _body = SimulatedResponseBodyType.bytes;
       } else {
         throw StateError('core has neither bytes or string body');
       }
     } else {
-      _body = _SimulatedResponseBody.none;
+      _body = SimulatedResponseBodyType.none;
     }
   }
 
@@ -101,7 +138,7 @@ class SimulatedResponse extends Response {
   //
   // When there is no body (e.g. with a redirect response), both are null.
 
-  late final _SimulatedResponseBody _body;
+  late final SimulatedResponseBodyType _body;
 
   late final String? _bodyStr; // store complete body
 
@@ -109,6 +146,14 @@ class SimulatedResponse extends Response {
 
   //================================================================
   // Methods
+
+  //----------------------------------------------------------------
+  /// The source format of the body.
+  ///
+  /// Indicates if there is a body, and (if there is) whether its source format
+  /// was a String or a sequence of bytes.
+
+  SimulatedResponseBodyType get bodyType => _body;
 
   //----------------------------------------------------------------
   /// Retrieves the body as a String.
@@ -127,46 +172,87 @@ class SimulatedResponse extends Response {
   //----------------------------------------------------------------
   /// Retrieves the body as a String.
   ///
-  /// The empty string is returned if there is no body.
+  /// If the source format of the body is a String, it is returned and the
+  /// _encoding_ is not used.
   ///
-  /// If the body is a sequence of bytes, it is converted to a string using
-  /// the decoder of the [encoding]. If the _encoding_ is `utf8`, this method
-  /// may throw a [FormatException] if the body contains a sequence of bytes
-  /// that do not represent a UTF-8 encoded code point.
+  /// If the source format of the body is a sequence of bytes, it is converted
+  /// to a String using the decoder of the [encoding]. Note: if the _encoding_
+  /// is `utf8`, this method may throw a [FormatException] when the bytes
+  /// do not represent a valid UTF-8 encoded code point.
+  ///
+  /// If there is no source for the body, returns the empty string.
+  ///
+  /// **Related methods**
   ///
   /// Use [bodyBytes] to retrieve the body as a sequence of bytes.
+  ///
   /// Use [bodyStr] as a shorthand for invoking this method with the [utf8]
   /// encoding.
+  ///
+  /// If retrieving the String to only see if it is empty, use [bodyIsEmpty]
+  /// instead (which works more efficiently if the body had been set to
+  /// a sequence of bytes, since it doesn't need to decode them).
 
   String bodyString(Encoding encoding) {
     switch (_body) {
-      case _SimulatedResponseBody.none:
-        return '';
-      case _SimulatedResponseBody.bytes:
+      case SimulatedResponseBodyType.none:
+        return ''; // empty String
+      case SimulatedResponseBodyType.bytes:
         return encoding.decode(_bodyBytes!);
-      case _SimulatedResponseBody.string:
+      case SimulatedResponseBodyType.string:
         return _bodyStr!;
     }
   }
 
   //----------------------------------------------------------------
-  /// Retrieves the body as a list of bytes.
+  /// Retrieves the body as a sequence of bytes.
   ///
-  /// The empty list is returned if there is no body.
+  /// If the source format of the body is a sequence of bytes, it is returned
+  /// and the _encoding_ is not used.
   ///
-  /// If the handler produced a String value, the UTF-8 encoding of that
-  /// value is returned.
+  /// If the source format of the body is a String, it is converted to a
+  /// sequence of bytes using the encoder of the [encoding].
   ///
-  /// Use [bodyStr] to retrieve the body as a String.
+  /// If there is no source for the body, returns an empty list.
+  ///
+  /// **Related methods**
+  ///
+  /// Use [bodyStr] or [bodyString] to retrieve the body as a String.
+  ///
+  /// If retrieving the bytes to only see if they are empty, use [bodyIsEmpty]
+  /// instead (which works more efficiently if the body had been set to a
+  /// String, since it doesn't need to encode it).
 
-  List<int> bodyBytes([Encoding enc = utf8]) {
+  List<int> bodyBytes(Encoding encoding) {
     switch (_body) {
-      case _SimulatedResponseBody.none:
+      case SimulatedResponseBodyType.none:
         return <int>[]; // empty list of bytes
-      case _SimulatedResponseBody.bytes:
+      case SimulatedResponseBodyType.bytes:
         return _bodyBytes!;
-      case _SimulatedResponseBody.string:
-        return enc.encode(_bodyStr!);
+      case SimulatedResponseBodyType.string:
+        return encoding.encode(_bodyStr!);
+    }
+  }
+
+  //----------------------------------------------------------------
+  /// Indicates if there is a body has content or not.
+  ///
+  /// Returns true if the body is non-empty (i.e. has been set to more than one
+  /// byte or a string with one or more characters).
+  ///
+  /// Returns true if the body has not been set, or has been set to zero bytes
+  /// or an empty string.
+
+  bool get bodyIsEmpty {
+    switch (_body) {
+      case SimulatedResponseBodyType.none:
+        return true;
+      case SimulatedResponseBodyType.bytes:
+        final b = _bodyBytes;
+        return b == null || b.isEmpty;
+      case SimulatedResponseBodyType.string:
+        final s = _bodyStr;
+        return s == null || s.isEmpty;
     }
   }
 
